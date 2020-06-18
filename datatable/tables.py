2f32e6eaeb0bea5eb037563b94059497296732e0
magia64 <brucepainter64@gmail.com>
2020-06-18 16:37:28 -0700

    update links to open in a new tab or window

--- datatable/tables.py
+++ datatable/tables.py
@@ -7,11 +7,11 @@ from django_tables2.utils import A
 class ProcurementTable(tables.Table):
     id = tables.Column(verbose_name='')
     actions = tables.LinkColumn('edit_procurement', args=[A('pk')], verbose_name='Actions', empty_values=(), text='Edit')
-    reqnumber = tables.TemplateColumn("<a href='https://eccprd.corp.amdocs.com/irj/portal/P2P?NavMode=10&req={{record.reqnumber}}'>{{record.reqnumber}}</a>")
+    reqnumber = tables.TemplateColumn("<a href='https://eccprd.corp.amdocs.com/irj/portal/P2P?NavMode=10&req={{record.reqnumber}}' target='_blank'>{{record.reqnumber}}</a>")
 
     prnumber = tables.TemplateColumn("""
     {% if record.prnumber != '0' %}
-    <a href='https://eccprd.corp.amdocs.com/irj/portal/P2P?NavMode=10&pr={{record.prnumber}}'>{{record.prnumber}}</a>
+    <a href='https://eccprd.corp.amdocs.com/irj/portal/P2P?NavMode=10&pr={{record.prnumber}}' target='_blank'>{{record.prnumber}}</a>
     {% endif %}
     {% if record.prnumber == '0' %}
     <a>__</a>
@@ -20,7 +20,7 @@ class ProcurementTable(tables.Table):
 
     ponumber = tables.TemplateColumn("""
     {% if record.ponumber != '0' %}
-    <a href='https://eccprd.corp.amdocs.com/irj/portal/P2P?NavMode=10&po={{record.ponumber}}'>{{record.ponumber}}</a>
+    <a href='https://eccprd.corp.amdocs.com/irj/portal/P2P?NavMode=10&po={{record.ponumber}}' target='_blank'>{{record.ponumber}}</a>
     {% endif %}
     {% if record.ponumber == '0' %}
     <a>__</a>
